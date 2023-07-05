bfd0d13ef12ca9417e94e3fdadeea65f7b5a8571
Abhimanyuap <97864658+Abhimanyuap@users.noreply.github.com>
2023-07-05 16:51:05 +0530

    Your commit message here

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Album from './components/Album';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+          <Album/>
+    </div>
+  );
+}
+
+export default App;
